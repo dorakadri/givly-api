98891260109febae866ef048148d05cd4b3f587d
dorakadri <dora.kadri@esprit.tn>
2023-04-30 21:34:36 +0100

    updated

--- controllers/diygeneration/diyCtrl.js
+++ controllers/diygeneration/diyCtrl.js
@@ -1,7 +1,28 @@
 const expressAsyncHandler = require("express-async-handler");
-const scrapeDiyCrafts = require("./diygeneration");
 
+const axios = require('axios');
+const cheerio = require('cheerio');
 
+async function scrapeDiyCrafts(objectName) {
+  const url = `https://www.diyncrafts.com/?s=${objectName.replace(' ', '+')}`;
+  const { data } = await axios.get(url);
+  const $ = cheerio.load(data);
+
+  const crafts = [];
+  $('article').each((i, el) => {
+    const title = $(el).find('h2').text().trim();
+    const link = $(el).find('a').attr('href');
+    let imgUrl = null;
+    const img = $(el).find('a.entry-image-link img');
+    if (img.length > 0) {
+      imgUrl = img.attr('src');
+    }
+
+    crafts.push({ title, link, imgUrl });
+  });
+
+  return JSON.stringify(crafts);
+}
 
 const Diyfetching = expressAsyncHandler(async (req, res) => {
     const { objectname } = req.params;
